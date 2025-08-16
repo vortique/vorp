8c513b674cd48d15060cf3aa5a044ca3b0298df6
Badi <wayw04321@gmail.com>
2025-08-16 19:35:04 +0300

    added if the specified path is a folder, the program gives necessary error information.

--- Vorp/Vorp.cs
+++ Vorp/Vorp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Reflection.Metadata;
 using System.Text;
 
 namespace Vorp
@@ -14,28 +15,35 @@ namespace Vorp
 
                 try
                 {
-                    FileInfo fileInfo = new FileInfo(filePath);
-                    long fileSize = fileInfo.Length;
-
-                    int bufferSize = GetBufferSize(fileSize);
-
-                    using (FileStream fs = new FileStream(filePath,
-                        FileMode.Open, FileAccess.Read))
+                    if (!Directory.Exists(filePath))
                     {
-                        UTF8Encoding utf8 = new UTF8Encoding();
-                        byte[] buffer = new byte[bufferSize];
-                        int bytesReaded;
-                        while ((bytesReaded = fs.Read(buffer, 0, buffer.Length)) > 0)
+                        FileInfo fileInfo = new FileInfo(filePath); // Getting file's info. Especially file's Length in bytes for dynamic buffer size.
+                        long fileSize = fileInfo.Length;
+
+                        int bufferSize = GetBufferSize(fileSize);
+
+                        using (FileStream fs = new FileStream(filePath,
+                            FileMode.Open, FileAccess.Read))
                         {
-                            String decodedText = utf8.GetString(buffer, 0, bytesReaded);
-                            Console.Write(decodedText);
+                            UTF8Encoding utf8 = new UTF8Encoding();
+                            byte[] buffer = new byte[bufferSize];
+                            int bytesReaded;
+                            while ((bytesReaded = fs.Read(buffer, 0, buffer.Length)) > 0)
+                            {
+                                String decodedText = utf8.GetString(buffer, 0, bytesReaded);
+                                Console.Write(decodedText);
+                            }
+                            Console.WriteLine();
                         }
-                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"vorp ({filePath}): Given path is a directory");
                     }
                 }
-                catch (FileNotFoundException)
+                catch (FileNotFoundException) // Exception handling when file not found
                 {
-                    Console.WriteLine($"vorp ({filePath}): No such file or directory");
+                    Console.WriteLine($"vorp ({filePath}): No such file");
                 }
                 catch (IOException e)
                 {
@@ -44,17 +52,18 @@ namespace Vorp
             }
         }
 
+        // Dynamic buffer size for optimizing RAM usage.
         static int GetBufferSize(long fileSize)
         {
-            if (fileSize < 1024)
+            if (fileSize < 1024) // 1 kB
             {
                 return 1024;
             }
-            else if (fileSize < 1024 * 1024)
+            else if (fileSize < 1024 * 1024) // 1 mB
             {
                 return 6 * 1024;
             }
-            else
+            else // Larger than 1 mB
             {
                 return 64 * 1024;
             }
